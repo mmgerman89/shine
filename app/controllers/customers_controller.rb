34c52f7e85d83438bec48fed999746708a61aef9
German <mmgerman89@gmail.com>
2016-08-23 21:27:34 -0300

    resultados de busqueda estilizada y paginacion

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,9 @@
 class CustomersController < ApplicationController
+  PAGE_SIZE = 10
+  
   def index
-  	@customers = Customer.all.limit(10)
+  	@page = (params[:page] || 0).to_i
+    @customers = Customer.all.limit(10)
 
   	if params[:keywords].present?
   		@keywords = params[:keywords]
@@ -8,7 +11,8 @@ class CustomersController < ApplicationController
   		@customers = Customer.where(
   			customer_search_term.where_clause,
   			customer_search_term.where_args,
-  			customer_search_term.order)
+  			customer_search_term.order).
+        offset(@page * PAGE_SIZE).limit(PAGE_SIZE)
   	else
   		@customers = []
   	end
